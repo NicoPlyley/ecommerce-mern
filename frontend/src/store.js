4bdba5fc112c75ef0daf7c792ee0dbe936f01713
Nico Plyley <nplyley@gmail.com>
2021-02-07 16:28:42 -0500

    Added list product reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,11 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
-import {produtListReducer} from './reducers/productReducers'
+import {produtListReducer, produtDetailsReducer} from './reducers/productReducers'
 
 const reducer = combineReducers({
-    productList: produtListReducer
+    productList: produtListReducer,
+    productDetails: produtDetailsReducer
 })
 
 const initialState = []
