92982ffff3d8b7537c5146f96240385966389c42
Nico Plyley <nplyley@gmail.com>
2021-02-10 14:38:19 -0500

    Added register to router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import {Container} from 'react-bootstrap'
 import {Header, Footer} from './components'
-import {CartScreen, HomeScreen, LoginScreen, ProductScreen} from './screens'
+import {CartScreen, HomeScreen, LoginScreen, ProductScreen, RegisterScreen} from './screens'
 
 const App = () => {
     return (
@@ -10,6 +10,7 @@ const App = () => {
             <main className="py-3">
                 <Container>
                     <Route path="/login" component={LoginScreen} />
+                    <Route path="/register" component={RegisterScreen} />
                     <Route path="/" component={HomeScreen} exact />
                     <Route path="/product/:id" component={ProductScreen} />
                     <Route path="/cart/:id?" component={CartScreen} />
