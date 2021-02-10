135e8b9a3d7c25572a7c9dc6752def22325ff97a
Nico Plyley <nplyley@gmail.com>
2021-02-10 16:19:23 -0500

    Added user details

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,14 +3,15 @@ import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import {produtListReducer, produtDetailsReducer} from './reducers/productReducers'
 import {cartReducer} from './reducers/CartReducers'
-import {userLoginReducer, userRegisterReducer} from './reducers/userReducers'
+import {userDetailsReducer, userLoginReducer, userRegisterReducer} from './reducers/userReducers'
 
 const reducer = combineReducers({
     productList: produtListReducer,
     productDetails: produtDetailsReducer,
     cart: cartReducer,
     userLogin: userLoginReducer,
-    userRegister: userRegisterReducer
+    userRegister: userRegisterReducer,
+    userDetails: userDetailsReducer
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
