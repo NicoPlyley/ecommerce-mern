00b5f8a00df6524b04f57dcabf389ed9dacea45e
Nico Plyley <nplyley@gmail.com>
2021-02-10 14:57:43 -0500

    Added update user functions

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,7 @@ import User from '../models/userModel.js'
 import generateToken from '../utils/generateToken.js'
 
 // @desc   Auth user & get token
-// @route  POST /api/user/login
+// @route  POST /api/users/login
 // @access Public
 export const authUser = asyncHandler(async (req, res) => {
     const {email, password} = req.body
@@ -59,7 +59,7 @@ export const reigtserUser = asyncHandler(async (req, res) => {
 })
 
 // @desc   Get user profile
-// @route  GET /api/user/profile
+// @route  GET /api/users/profile
 // @access Private
 export const getUserProfile = asyncHandler(async (req, res) => {
     const user = await User.findById(req.user._id)
@@ -71,6 +71,35 @@ export const getUserProfile = asyncHandler(async (req, res) => {
             email: user.email,
             isAdmin: user.isAdmin
         })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
+// @desc   Update user profile
+// @route  PUT /api/users/profile
+// @access Private
+export const updateUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+
+        if (req.body.password) {
+            user.password = req.body.password
+        }
+
+        const updatedUser = await user.save()
+
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin
+        })
+
     } else {
         res.status(404)
         throw new Error('User not found')
