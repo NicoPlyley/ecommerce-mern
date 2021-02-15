ed17c8fa2c27461ba04a9571b4667788e10f4229
Nico Plyley <nplyley@gmail.com>
2021-02-15 14:43:24 -0500

    Added order routes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import colors from 'colors'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
-import {notFound, errorHandler} from './middleware/errorMiddleware.js'
+import orderRoutes from './routes/orderRoutes'
+import {errorHandler, notFound} from './middleware/errorMiddleware.js'
 
 dotenv.config()
 connectDB()
@@ -15,6 +15,7 @@ app.use(express.json())
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
