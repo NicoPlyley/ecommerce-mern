fb481e2a504fd4c67a4e82b3016c962257d8f0af
Nico Plyley <nplyley@gmail.com>
2021-02-15 20:30:15 -0500

    Created reset on logout

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -4,7 +4,12 @@ import {
     ORDER_CREATE_SUCCESS,
     ORDER_DETAILS_FAIL,
     ORDER_DETAILS_REQUEST,
-    ORDER_DETAILS_SUCCESS, ORDER_PAY_FAIL, ORDER_PAY_REQUEST, ORDER_PAY_SUCCESS
+    ORDER_DETAILS_SUCCESS, ORDER_LIST_USERS_FAIL,
+    ORDER_LIST_USERS_REQUEST,
+    ORDER_LIST_USERS_SUCCESS,
+    ORDER_PAY_FAIL,
+    ORDER_PAY_REQUEST,
+    ORDER_PAY_SUCCESS
 } from '../constants/orderConstants'
 import axios from 'axios'
 
@@ -92,6 +97,36 @@ export const payOrder = (orderId, paymentResult) => async (dispatch, getState) =
     } catch (error) {
         dispatch({
             type: ORDER_PAY_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message
+        })
+    }
+}
+
+export const listUserOrders = () => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: ORDER_LIST_USERS_REQUEST
+        })
+
+        const {userLogin: {userInfo}} = getState()
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const {data} = await axios.get('/api/orders/myorders', config)
+
+        dispatch({
+            type: ORDER_LIST_USERS_SUCCESS,
+            payload: data
+        })
+
+    } catch (error) {
+        dispatch({
+            type: ORDER_LIST_USERS_FAIL,
             payload: error.response && error.response.data.message ? error.response.data.message : error.message
         })
     }
--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -4,7 +4,13 @@ import {
     ORDER_CREATE_SUCCESS,
     ORDER_DETAILS_FAIL,
     ORDER_DETAILS_REQUEST,
-    ORDER_DETAILS_SUCCESS, ORDER_PAY_FAIL, ORDER_PAY_REQUEST, ORDER_PAY_RESET, ORDER_PAY_SUCCESS
+    ORDER_DETAILS_SUCCESS,
+    ORDER_LIST_USERS_FAIL, ORDER_LIST_USERS_REQUEST, ORDER_LIST_USERS_RESET,
+    ORDER_LIST_USERS_SUCCESS,
+    ORDER_PAY_FAIL,
+    ORDER_PAY_REQUEST,
+    ORDER_PAY_RESET,
+    ORDER_PAY_SUCCESS
 } from '../constants/orderConstants'
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -69,6 +75,31 @@ export const orderPayReducer = (state = {}, action) => {
             }
         case ORDER_PAY_RESET:
             return {}
+        default:
+            return state
+    }
+}
+
+export const orderListUsersReducer = (state = {orders: []}, action) => {
+    switch (action.type) {
+        case ORDER_LIST_USERS_REQUEST:
+            return {
+                loading: true
+            }
+        case ORDER_LIST_USERS_SUCCESS:
+            return {
+                loading: false,
+                orders: action.payload
+            }
+        case ORDER_LIST_USERS_FAIL:
+            return {
+                loading: false,
+                error: action.payload
+            }
+        case ORDER_LIST_USERS_RESET:
+            return {
+                orders: []
+            }
         default:
             return state
     }
