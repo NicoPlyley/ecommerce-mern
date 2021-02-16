89ea25c992bd8e34930407613bdd37264c3e504b
Nico Plyley <nplyley@gmail.com>
2021-02-16 16:10:42 -0500

    Added product list to routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,38 +2,40 @@ import {BrowserRouter as Router, Route} from 'react-router-dom'
 import {Container} from 'react-bootstrap'
 import {Header, Footer} from './components'
 import {
-    CartScreen,
-    HomeScreen,
-    LoginScreen, OrderScreen, PaymentScreen, PlaceOrderScreen,
-    ProductScreen,
-    ProfileScreen,
-    RegisterScreen,
-    ShippingScreen, UserEditScreen, UserListScreen
+  CartScreen,
+  HomeScreen,
+  LoginScreen, OrderScreen, PaymentScreen, PlaceOrderScreen, ProductListScreen,
+  ProductScreen,
+  ProfileScreen,
+  RegisterScreen,
+  ShippingScreen, UserEditScreen, UserListScreen
 } from './screens'
 
 const App = () => {
-    return (
-        <Router>
-            <Header />
-            <main className="py-3">
-                <Container>
-                    <Route path="/login" component={LoginScreen} />
-                    <Route path="/register" component={RegisterScreen} />
-                    <Route path="/profile" component={ProfileScreen} />
-                    <Route path="/shipping" component={ShippingScreen} />
-                    <Route path="/payment" component={PaymentScreen} />
-                    <Route path="/placeorder" component={PlaceOrderScreen} />
-                    <Route path="/order/:id" component={OrderScreen} />
-                    <Route path="/" component={HomeScreen} exact />
-                    <Route path="/product/:id" component={ProductScreen} />
-                    <Route path="/cart/:id?" component={CartScreen} />
-                    <Route path="/admin/users" component={UserListScreen} />
-                    <Route path="/admin/user/:id/edit" component={UserEditScreen} />
-                </Container>
-            </main>
-            <Footer />
-        </Router>
-    )
+  return (
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path="/login" component={LoginScreen} />
+          <Route path="/register" component={RegisterScreen} />
+          <Route path="/profile" component={ProfileScreen} />
+          <Route path="/shipping" component={ShippingScreen} />
+          <Route path="/payment" component={PaymentScreen} />
+          <Route path="/placeorder" component={PlaceOrderScreen} />
+          <Route path="/order/:id" component={OrderScreen} />
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/cart/:id?" component={CartScreen} />
+          <Route path="/admin/users" component={UserListScreen} />
+          <Route path="/admin/user/:id/edit" component={UserEditScreen} />
+          <Route path="/admin/products" component={ProductListScreen} />
+          {/*<Route path="/admin/product/:id/edit" component={ProductListScreen} />*/}
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  )
 }
 
 export default App
