ee0865d224b31ae0bc8ecbca39e074e6876c69ae
Nico Plyley <nplyley@gmail.com>
2021-02-16 23:53:11 -0500

    Added search functionality

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -4,15 +4,17 @@ import {Col, Row} from 'react-bootstrap'
 import {Product, Message, Loader} from '../components'
 import {listProducts} from '../actions/productActions.js'
 
-const HomeScreen = () => {
+const HomeScreen = ({match}) => {
+    const keyword = match.params.keyword
+
     const dispatch = useDispatch()
 
     const productList = useSelector(state => state.productList)
     const {loading, error, products} = productList
 
     useEffect(() => {
-        dispatch(listProducts())
-    }, [dispatch])
+        dispatch(listProducts(keyword))
+    }, [dispatch, keyword])
 
     return (
         <>
