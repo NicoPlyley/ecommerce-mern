444a7f15428699d3eb807021685ef2b8deb5ac84
Nico Plyley <nplyley@gmail.com>
2021-02-16 20:08:06 -0500

    Added get orders

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,18 @@
 import express from 'express'
 const router = express.Router()
-import {addOrderItems, getCurrentUsersOrders, getOrderById, updateOrderToPaid} from '../controllers/orderController.js'
-import {protect} from '../middleware/authMiddleware.js'
+import {
+  addOrderItems,
+  getCurrentUsersOrders,
+  getOrderById,
+  getOrders,
+  updateOrderToPaid
+} from '../controllers/orderController.js'
+import {admin, protect} from '../middleware/authMiddleware.js'
 
-router.route('/').post(protect, addOrderItems)
+router.route('/')
+  .post(protect, addOrderItems)
+  .get(protect, admin, getOrders)
+
 router.route('/myorders').get(protect, getCurrentUsersOrders)
 router.route('/:id').get(protect, getOrderById)
 router.route('/:id/pay').put(protect, updateOrderToPaid)
