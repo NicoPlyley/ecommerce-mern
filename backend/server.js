b176de1db20ef2afb87ae072f4b88491fce94cea
Nico Plyley <nplyley@gmail.com>
2021-02-17 03:30:36 -0500

    Added deployment server

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,14 @@ app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_I
 
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend/build/index.html'))
+  })
+}
+
 app.use(notFound)
 app.use(errorHandler)
 
