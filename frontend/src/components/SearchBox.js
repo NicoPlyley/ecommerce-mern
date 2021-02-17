b982faf5cf2f8499984b78ec65efa350b91b5956
Nico Plyley <nplyley@gmail.com>
2021-02-16 23:52:30 -0500

    Created search box

---
+++ frontend/src/components/SearchBox.js
@@ -0,0 +1,39 @@
+import {useEffect, useState} from 'react'
+import {Button, Form} from 'react-bootstrap'
+
+const SearchBox = ({history}) => {
+  const [keyword, setKeyword] = useState('')
+
+  useEffect(() => {
+    if(keyword.trim()) {
+      history.push(`/search/${keyword}`)
+    } else {
+      history.push('/')
+    }
+  }, [keyword])
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    if(keyword.trim()) {
+      history.push(`/search/${keyword}`)
+    } else {
+      history.push('/')
+    }
+  }
+
+  return (
+    <Form onSubmit={submitHandler} inline>
+      <Form.Control
+        type="text"
+        name="q"
+        value={keyword}
+        onChange={e => setKeyword(e.target.value)}
+        placeholder="Search Products..."
+        className="mr-sm-2 ml-sm-5"
+      />
+      <Button type="submit" variant="outline-success" className="p-2">Search</Button>
+    </Form>
+  )
+}
+
+export default SearchBox
